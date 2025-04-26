52c8cd9866b4a8cdc5f95f0ed0822efc1c47e846
Hayden Bleasel <hello@haydenbleasel.com>
2025-04-26 13:37:21 -0700

    Fix hydration issue

--- components/canvas/index.tsx
+++ components/canvas/index.tsx
@@ -1,8 +1,11 @@
+import { ClientOnly } from '@/providers/client';
 import { ReactFlowProvider } from '@xyflow/react';
 import { CanvasInner, type CanvasProps } from './inner';
 
 export const Canvas = ({ projects, data, userId }: CanvasProps) => (
   <ReactFlowProvider>
-    <CanvasInner projects={projects} data={data} userId={userId} />
+    <ClientOnly>
+      <CanvasInner projects={projects} data={data} userId={userId} />
+    </ClientOnly>
   </ReactFlowProvider>
 );
