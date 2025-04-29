cdd5ae7628224c5a429e86e4fd21b006149df544
Hayden Bleasel <hello@haydenbleasel.com>
2025-04-29 14:39:48 -0700

    Remove background

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,20 +1,12 @@
-import Image from 'next/image';
 import type { ReactNode } from 'react';
-import Background from './background.jpg';
 
 type AuthLayoutProps = {
   readonly children: ReactNode;
 };
 
 const AuthLayout = ({ children }: AuthLayoutProps) => (
-  <div className="relative flex h-screen w-full items-center justify-center p-8">
-    <Image
-      src={Background}
-      alt="Background"
-      className="absolute inset-0 size-full object-cover"
-      placeholder="blur"
-    />
-    <div className="relative z-10 size-full">{children}</div>
+  <div className="relative flex h-screen w-full items-center justify-center bg-secondary">
+    <div className="relative z-10 size-full p-8">{children}</div>
   </div>
 );
 
