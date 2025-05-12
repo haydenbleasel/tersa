a70671e1295aa0fff763a1e9694b69fc8779b044
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-11 22:33:22 -0700

    Resolves #42

--- components/nodes/toolbar.tsx
+++ components/nodes/toolbar.tsx
@@ -1,5 +1,3 @@
-import { useUser } from '@/hooks/use-user';
-import { useProject } from '@/providers/project';
 import { NodeToolbar as NodeToolbarRaw, useReactFlow } from '@xyflow/react';
 import { Position } from '@xyflow/react';
 import { Fragment, type ReactNode } from 'react';
@@ -18,13 +16,10 @@ type NodeToolbarProps = {
 export const NodeToolbar = ({ id, items }: NodeToolbarProps) => {
   const { getNode } = useReactFlow();
   const node = getNode(id);
-  const { project } = useProject();
-  const user = useUser();
-  const editable = project?.userId === user?.id;
 
   return (
     <NodeToolbarRaw
-      isVisible={node?.selected && editable}
+      isVisible={node?.selected}
       position={Position.Bottom}
       className="flex items-center gap-1 rounded-full bg-background/40 p-1.5 backdrop-blur-sm"
     >
