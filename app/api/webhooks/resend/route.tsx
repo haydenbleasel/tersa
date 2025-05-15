ea71ab8a36c174393f0c1619c777f3d94a4b8770
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-14 17:54:41 -0700

    Update route.tsx

--- app/api/webhooks/resend/route.tsx
+++ app/api/webhooks/resend/route.tsx
@@ -1,61 +1,68 @@
-import ForgotPasswordEmailTemplate from '@/emails/forgot-password';
+import { ForgotPasswordEmailTemplate } from '@/emails/forgot-password';
 import { LoginEmailTemplate } from '@/emails/sign-in';
 import { SignupEmailTemplate } from '@/emails/sign-up';
 import { env } from '@/lib/env';
 import { parseError } from '@/lib/error/parse';
-import { render } from '@react-email/components';
+import { resend } from '@/lib/resend';
 import { NextResponse } from 'next/server';
 import type { ReactElement } from 'react';
-import { Resend } from 'resend';
 import { Webhook } from 'standardwebhooks';
 
-export const POST = async (req: Request) => {
-  const resend = new Resend(env.RESEND_TOKEN);
+type WebhookPayload = {
+  user: {
+    email: string;
+  };
+  email_data: {
+    token: string;
+    token_hash: string;
+    redirect_to: string;
 
+    // https://github.com/supabase/auth/blob/master/internal/mailer/template.go#L56-L66
+    email_action_type:
+      | 'signup'
+      | 'recovery'
+      | 'invite'
+      | 'magiclink'
+      | 'email_change'
+      | 'email'
+      | 'email_change_current'
+      | 'email_change_new'
+      | 'reauthentication';
+    site_url: string;
+    token_new: string;
+    token_hash_new: string;
+  };
+};
+
+export const POST = async (req: Request) => {
   try {
     const payload = await req.text();
+
+    console.log(payload, typeof payload, 'payload');
+
     const headers = Object.fromEntries(req.headers);
+
     const wh = new Webhook(env.SUPABASE_AUTH_HOOK_SECRET);
+
+    console.log(wh, typeof wh, 'wh');
 
     const {
       user,
       email_data: { token, token_hash, redirect_to, email_action_type },
-    } = wh.verify(payload, headers) as {
-      user: {
-        email: string;
-      };
-      email_data: {
-        token: string;
-        token_hash: string;
-        redirect_to: string;
+    } = wh.verify(payload, headers) as WebhookPayload;
 
-        // https://github.com/supabase/auth/blob/master/internal/mailer/template.go#L56-L66
-        email_action_type:
-          | 'signup'
-          | 'recovery'
-          | 'invite'
-          | 'magiclink'
-          | 'email_change'
-          | 'email'
-          | 'email_change_current'
-          | 'email_change_new'
-          | 'reauthentication';
-        site_url: string;
-        token_new: string;
-        token_hash_new: string;
-      };
-    };
+    console.log(user, typeof user, 'user');
 
     const magicLink = new URL(redirect_to, env.NEXT_PUBLIC_SUPABASE_URL);
 
     magicLink.searchParams.set('token', token);
     magicLink.searchParams.set('token_hash', token_hash);
 
-    let emailComponent: ReactElement | undefined;
+    let react: ReactElement | undefined;
     let subject: string | undefined;
 
     if (email_action_type === 'signup') {
-      emailComponent = (
+      react = (
         <SignupEmailTemplate
           magicLink={magicLink.toString()}
           email={user.email}
@@ -63,7 +70,7 @@ export const POST = async (req: Request) => {
       );
       subject = 'Confirm your email address for Tersa';
     } else if (email_action_type === 'magiclink') {
-      emailComponent = (
+      react = (
         <LoginEmailTemplate
           magicLink={magicLink.toString()}
           email={user.email}
@@ -71,7 +78,7 @@ export const POST = async (req: Request) => {
       );
       subject = 'Your magic link to login to Tersa';
     } else if (email_action_type === 'recovery') {
-      emailComponent = (
+      react = (
         <ForgotPasswordEmailTemplate
           magicLink={magicLink.toString()}
           email={user.email}
@@ -82,13 +89,13 @@ export const POST = async (req: Request) => {
       throw new Error('Invalid email action type');
     }
 
-    const html = await render(emailComponent);
+    console.log(react, typeof react, 'react');
 
     const { error } = await resend.emails.send({
       from: env.RESEND_EMAIL,
       to: [user.email],
       subject,
-      html,
+      react,
     });
 
     if (error) {
