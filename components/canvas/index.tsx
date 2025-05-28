f7f739d10cbce7283a8f9da53d355dc1875b4333
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-28 11:56:34 -0700

    Flatten canvas

--- components/canvas/index.tsx
+++
@@ -1,4 +0,0 @@
-import type { ReactFlowProps } from '@xyflow/react';
-import { Canvas as CanvasComponent } from './canvas';
-
-export const Canvas = (props: ReactFlowProps) => <CanvasComponent {...props} />;
